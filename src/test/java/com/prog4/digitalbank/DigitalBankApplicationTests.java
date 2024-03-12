228842fd3af273e37e1d9143819259282f4c52b9
unknown <hei.tsiory@gmail.com>
2024-03-12 07:36:55 +0300

    project initialisation

---
+++ src/test/java/com/prog4/digitalbank/DigitalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog4.digitalbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
