26d1f3313275a20b6a1f25358e89b8244a0f13aa
unknown <hei.tsiory@gmail.com>
2024-03-22 11:31:01 +0300

    feat : add error message on provisioning

--- src/main/java/com/prog4/digitalbank/provising/ProvisingServices.java
+++ src/main/java/com/prog4/digitalbank/provising/ProvisingServices.java
@@ -35,9 +35,14 @@ public class ProvisingServices {
                 accountId
         );
 
-        Provisioning insert = provisingSave.insert(provisioningToInsert);
-        String getTransactionId = insertServices.insertTransaction(accountId , amount , effective , "credit" , id , "provisioning");
-        insertServices.upDateAndInsertBalances(accountId , amount , effective , getTransactionId);
-        return insert;
+        if(effective.before(Date.valueOf(LocalDate.now()))){
+             Provisioning error = new Provisioning("the effective date must be after today");
+             return error;
+        }else {
+            Provisioning insert = provisingSave.insert(provisioningToInsert);
+            String getTransactionId = insertServices.insertTransaction(accountId, amount, effective, "credit", id, "provisioning");
+            insertServices.upDateAndInsertBalances(accountId, amount, effective, getTransactionId);
+            return insert;
+        }
     }
 }
