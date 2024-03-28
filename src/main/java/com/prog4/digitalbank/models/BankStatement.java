26c8f8f51e701a83815c10dda1c5db724e835de5
unknown <hei.tsiory@gmail.com>
2024-03-29 00:27:54 +0300

    feat : endpoint to get the bankStatement by account id and by month

---
+++ src/main/java/com/prog4/digitalbank/models/BankStatement.java
@@ -0,0 +1,19 @@
+package com.prog4.digitalbank.models;
+
+import lombok.*;
+
+import java.sql.Date;
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+public class BankStatement {
+    private Date date;
+    private String transactionRef;
+    private String operation;
+    private Double amount;
+    private Double balance;
+    private String pattern;
+}
