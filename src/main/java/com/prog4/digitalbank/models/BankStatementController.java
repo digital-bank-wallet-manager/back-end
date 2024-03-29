e219b5c52579d0407302ba17d9c0ac89bcee248a
unknown <hei.tsiory@gmail.com>
2024-03-29 16:50:47 +0300

    feat : generate a pdf to bankStatement

--- src/main/java/com/prog4/digitalbank/models/BankStatementController.java
+++ src/main/java/com/prog4/digitalbank/models/BankStatementController.java
@@ -1,10 +1,16 @@
 package com.prog4.digitalbank.models;
 
+import com.itextpdf.text.DocumentException;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -14,5 +20,16 @@ public class BankStatementController {
     @GetMapping("/bankStatement/{accountId}/{month}")
     public List<BankStatement> bankStatements (@PathVariable String accountId ,@PathVariable int month){
         return bankStatementServices.bankStatements(accountId , month);
+    }
+
+    @GetMapping(value = "/download/{accountId}/{month}" , produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<ByteArrayResource> downloadPDF(@PathVariable String accountId ,@PathVariable int month) throws DocumentException {
+        byte[] pdfBytes = bankStatementServices.generatePdf(accountId , month);
+        ByteArrayResource resource = new ByteArrayResource(pdfBytes);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename="+accountId+".pdf");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
     }
 }
